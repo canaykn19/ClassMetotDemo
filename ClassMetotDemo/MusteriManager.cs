c6645d34b8633333ce81e3f63ddf5435dc0a8e2f
HP <HP@DESKTOP-B0EOT6B>
2022-12-05 15:34:04 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi." + ":" + musteri.name + "  " + musteri.surname + "  " + musteri.id);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi." + ":" + musteri.name + "  " + musteri.surname);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri." + ":" + musteri.name + "  " +  musteri.surname );
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri=new Musteri();
+            musteri.id = 1;
+            musteri.name = "Can";
+            musteri.surname = "Aykın";
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.id = 2;
+            musteri2.name = "İrem";
+            musteri2.surname = "Teke";
+            
+            Musteri musteri3 = new Musteri();
+            musteri3.id = 3;
+            musteri3.name = "Enes";
+            musteri3.surname = "Şahin";
+
+           
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri3);
+            Console.WriteLine("--------------------");
+            musteriManager.Sil(musteri);
+            Console.WriteLine("--------------------");
+            musteriManager.Listele(musteri2);
+            Console.WriteLine("--------------------");
+            Console.ReadKey();
+        }
+
+    }
+}
